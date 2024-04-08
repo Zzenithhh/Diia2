c3e9284bfadcacf47d40da25251e59e93bf6f24e
Zenithhh <93765870+Zzenithhh@users.noreply.github.com>
2024-04-08 20:54:32 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -1,24 +1,24 @@
 // Активировать документы (true - включены, false - отключены)
-var isRightsEnabled = true; // Права
-var isCovidCertificateEnabled = true; // COVID-сертификат
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var birthdate = "04.04.2006"; // Дата рождения
+  var fullname = "Мирка Ярослав Юрійович" // ФИО
+var name = "Ярослав" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "007101921" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "3917502723" // ИНН
 
 // Права
-var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_valid_until = "21.06.2027"
 var rights_categories = "A, B" // Права "Категорії"
 var rights_tsc = "ТСЦ 8631" // Права "Видав
 var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
-var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_valid_until = "04.08.2022" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
